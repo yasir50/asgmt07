7f3e05d88736e07310f33f09d02458afa24b8e48
yasir raza <yraza3859@gmail.com>
2024-12-22 17:40:32 +0500

    realme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
-
+import "./App.css";
+import Cards from "./Cards";
+import Carousel from "./Carousel";
+import Footer from "./Footer";
+import Navbar from "./navbar";
 function App() {
-
   return (
     <>
+      <Navbar />
+      <Carousel />
+      <h2 className="smartPhones">SmartPhones</h2>
+      <Cards />
+      <Footer />
     </>
-  )
+  );
 }
-console.log("running")
 
-export default App
+export default App;
